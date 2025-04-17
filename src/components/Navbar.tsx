7682730896416d0e104a90d83fc25616bae149ab
= <=>
2025-04-16 23:58:22 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { MdMyLocation, MdWbSunny } from 'react-icons/md'
+import { SlLocationPin } from 'react-icons/sl'
+import SearchBox from './SearchBox'
+
+type Props = {}
+
+export default function Navbar({}: Props) {
+  return (
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+            <div className='flex items-center justify-center gap-2'>
+                <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                <MdWbSunny className='text-3xl mt-1 text-yellow-400' />
+            </div>
+            <div className='flex gap-4 items-center'>
+                <MdMyLocation className='text-2xl text-gray-500 hover:opacity-80 cursor-pointer' />
+                <SlLocationPin className='text-3xl text-gray-500 hover:opacity-80 cursor-pointer' />
+                <p className='text-slate-900/80 text-sm'>Santo Tomé</p>
+                <div>
+                    <SearchBox />
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils/cn';
+import React from 'react'
+import { IoSearch } from 'react-icons/io5'
+
+type Props = {
+    className?: string;
+    value: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+    onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
+
+export default function SearchBox(props: Props) {
+  return (
+    <form 
+        onSubmit={props.onSubmit}
+        className={cn('flex relative items-center justify-center h-10', props.className)}
+    >
+        <input
+            onChange={props.onChange}
+            value={props.value}
+            type='text' 
+            placeholder='Seach location...' 
+            className='px-4 py-2 w-[200px] border border-gray-300 rounded-l-xl focus:outline-none focus:border-blue-500 h-full'
+        />
+        <button className='px-4 py-[10px] bg-blue-500 text-white rounded-r-xl focus:outline-none hover:bg-blue-500 whitespace-nowrap h-full'>
+            <IoSearch />
+        </button>
+    </form>
+  )
+}
