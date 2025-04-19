cf9e0a0fa97ec61500eb44a1fbf6d9193c0f2a20
= <=>
2025-04-19 17:24:57 -0300

    feat: enhance ForecastWeatherDetail component with weather data display and styling updates

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import Container from './Container'
+import WeatherIcon from './WeatherIcon'
+import { WeatherDetailsProps } from './WeatherDetails';
+import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelsius';
 
 type Props = {}
 
-export default function ForecastWeatherDetail({}: Props) {
+export interface ForecastWeatherDetailProps extends WeatherDetailsProps {
+  weatherIcon: string;
+  date: string;
+  day: string;
+  temperature: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  description: string;
+}
+
+export default function ForecastWeatherDetail(
+  props: ForecastWeatherDetailProps
+) {
+
+  const {
+    weatherIcon = '01d',
+    date = '31 Dec 2025',
+    day = 'Monday',
+    temperature,
+    feels_like,
+    temp_min,
+    temp_max,
+    description,
+  } = props;
+
   return (
-    <Container   />
+    <Container className='gap-4'>
+      {/* Left section */}
+      <section className='flex gap-4 items-center px-4'>
+        {/* Weather icon and date */}
+        <div>
+          <WeatherIcon
+            iconname={weatherIcon}
+          />
+          <p>{date}</p>
+          <p className='text-sm'>{day}</p>
+        </div>
+
+        {/* Weather details */}
+        <div className='flex flex-col px-4'>
+          <span className='text-5xl'>{convertKelvinToCelsius(temperature ?? 0)}°</span>
+          <p className='texst-xs space-x-1 whitespace-nowrap'>
+            <span>Sensación térmica: </span>
+            <span>{convertKelvinToCelsius(feels_like ?? 0)}°</span>
+          </p>
+          <p className='capitalize'>{description}</p>
+        </div>
+      </section>
+
+      {/* Right section */}
+      <section className='flex overflow-x-auto justify-between gap-4 px-4 w-full pr-10'>
+        
+      </section>
+    </Container>
   )
 }
--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -69,7 +69,7 @@ export interface SingleWeatherDetailProps {
 
 function SingleWeatherDetail (props: SingleWeatherDetailProps) {
     return (
-        <div className='flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black/80'>
+        <div className='flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black/60'>
             <p className='whitespace-nowrap'>{props.information}</p>
             <div className='text-3xl'>{props.icon}</div>
             <p>{props.value}</p>
