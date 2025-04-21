487aba93b400e70a30de2b86056de569c7c6a34b
= <=>
2025-04-20 22:07:09 -0300

    refactor: move QueryClient logic to QueryProvider for better separation of concerns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,24 @@
-'use client';
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import QueryProvider from "./QueryProvider";
 
-
-// export const metadata: Metadata = {
-//   title: "Weather App",
-//   description: "A simple weather app with Next.js and Tailwind CSS",
-// };
+export const metadata: Metadata = {
+  title: "Weather App",
+  description: "A simple weather app with Next.js and Tailwind CSS",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient();
-
   return (
     <html lang="es">
-      <QueryClientProvider client={queryClient}>
+      <QueryProvider>
         <body>
           {children}
         </body>
-      </QueryClientProvider>
+      </QueryProvider>
     </html>
   );
 }
